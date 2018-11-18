cd2f6d95a49c5e011e436e15ff220d7e68ba1dd7
Aki <aki.y.ishikawa@gmail.com>
2018-11-18 09:43:16 +0900

    feature: Added batchnorm layer in LeNet

--- model.py
+++ model.py
@@ -3,6 +3,8 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+
+
 class QNetwork(nn.Module):
     """Actor (Policy) Model."""
 
@@ -43,20 +45,24 @@ class LeNet(nn.Module):
             fc2_units (int): Number of nodes in second hidden layer
         """
         super().__init__()
+        dropout_rate = 0.0
         self.seed = torch.manual_seed(seed)
         self.conv1 = nn.Conv2d(3, 6, 5)
+        self.bn1 = nn.BatchNorm2d(6)
         self.conv2 = nn.Conv2d(6, 16, 5)
+        self.bn2 = nn.BatchNorm2d(16)
         self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.dropout = nn.Dropout(dropout_rate)
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, action_size)
 
     def forward(self, x):
         """Build a network that maps state -> action values."""
         x = x.view(-1, 3, 32, 32)
-        x = F.max_pool2d(F.relu(self.conv1(x)), 2)
-        x = F.max_pool2d(F.relu(self.conv2(x)), 2)
+        x = F.max_pool2d(F.relu(self.bn1(self.conv1(x))), 2)
+        x = F.max_pool2d(F.relu(self.bn2(self.conv2(x))), 2)
         x = x.view(-1, self.num_flat_features(x))
-        x = F.relu(self.fc1(x))
+        x = self.dropout(F.relu(self.fc1(x)))
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
         return x
